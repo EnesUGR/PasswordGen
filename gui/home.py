8785b7180095aca4384836d3a8c542dc60a1503f
Enes <eugurx@gmail.com>
2023-08-09 00:05:49 +0300

    Check page added

--- gui/home.py
+++ gui/home.py
@@ -3,6 +3,7 @@ import sys
 from PySide6.QtWidgets import QApplication,QMainWindow
 from gui.home_python import Ui_MainWindow
 from generating_password import generate_password
+from checking_password import Checking
 
 class Home(QMainWindow):
     def __init__(self):
@@ -12,6 +13,7 @@ class Home(QMainWindow):
         self.ui.pushButton_generatePassword.clicked.connect(self.generate)
         self.ui.toolButton_copy.clicked.connect(self.copy_password)
         self.ui.pushButton_check.clicked.connect(self.check)
+        self.ui.pushButton_gohome.clicked.connect(lambda : self.ui.stackedWidget.setCurrentWidget(self.ui.page_home))
 
     def copy_password(self):
         self.ui.lineEdit_password.selectAll()
@@ -24,6 +26,23 @@ class Home(QMainWindow):
 
     def check(self):
         self.ui.stackedWidget.setCurrentWidget(self.ui.page_check)
+        pw = self.ui.lineEdit_password.text()
+        ch = Checking(password_text=pw)
+        ch_details = ch.get_detailes()
+        self.ui.labelChecking_password.setText(pw)
+        self.ui.labelChecking_score.setText(ch_details["score"])
+        self.ui.labelChecking_eabits.setText(ch_details["eabits"])
+        self.ui.labelChecking_weakFactor.setText(ch_details["weak"])
+        self.ui.labelChecking_length.setText(ch_details["length"])
+        self.ui.labelChecking_letters.setText(ch_details["letters"])
+        self.ui.labelChecking_uppercase.setText(ch_details["uppercase"])
+        self.ui.labelChecking_lowercase.setText(ch_details["lowercase"])
+        self.ui.labelChecking_specialcase.setText(ch_details["specialcase"])
+        self.ui.labelChecking_nums.setText(ch_details["nums"])
+        self.ui.labelChecking_repeated.setText(ch_details["repeatedpattern"])
+        # self.ui.labelChecking_chars.setText(ch_details["chars"])
+        self.ui.labelChecking_combs.setText(ch_details["combs"])
+
 
 
 
--- gui/home_python.py
+++ gui/home_python.py
@@ -16,10 +16,10 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLineEdit, QMainWindow,
-    QMenu, QMenuBar, QPushButton, QSizePolicy,
-    QSpacerItem, QStackedWidget, QStatusBar, QToolButton,
-    QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QLabel,
+    QLineEdit, QMainWindow, QMenu, QMenuBar,
+    QPushButton, QSizePolicy, QSpacerItem, QStackedWidget,
+    QStatusBar, QToolButton, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -94,6 +94,204 @@ class Ui_MainWindow(object):
         self.stackedWidget.addWidget(self.page_home)
         self.page_check = QWidget()
         self.page_check.setObjectName(u"page_check")
+        self.verticalLayout_3 = QVBoxLayout(self.page_check)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.labelChecking_password = QLabel(self.page_check)
+        self.labelChecking_password.setObjectName(u"labelChecking_password")
+        self.labelChecking_password.setMinimumSize(QSize(0, 10))
+        self.labelChecking_password.setMaximumSize(QSize(16777215, 20))
+        font2 = QFont()
+        font2.setBold(True)
+        self.labelChecking_password.setFont(font2)
+        self.labelChecking_password.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_3.addWidget(self.labelChecking_password)
+
+        self.gridLayout_check = QGridLayout()
+        self.gridLayout_check.setObjectName(u"gridLayout_check")
+        self.label_5 = QLabel(self.page_check)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setFont(font1)
+        self.label_5.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.label_5, 2, 0, 1, 1)
+
+        self.label_3 = QLabel(self.page_check)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setFont(font1)
+        self.label_3.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.label_3, 0, 1, 1, 1)
+
+        self.label_6 = QLabel(self.page_check)
+        self.label_6.setObjectName(u"label_6")
+        self.label_6.setFont(font1)
+        self.label_6.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.label_6, 2, 1, 1, 1)
+
+        self.label_13 = QLabel(self.page_check)
+        self.label_13.setObjectName(u"label_13")
+        self.label_13.setFont(font1)
+        self.label_13.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.label_13, 0, 4, 1, 1)
+
+        self.labelChecking_letters = QLabel(self.page_check)
+        self.labelChecking_letters.setObjectName(u"labelChecking_letters")
+        font3 = QFont()
+        font3.setPointSize(12)
+        font3.setBold(True)
+        font3.setItalic(False)
+        self.labelChecking_letters.setFont(font3)
+        self.labelChecking_letters.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.labelChecking_letters, 3, 0, 1, 1)
+
+        self.label_12 = QLabel(self.page_check)
+        self.label_12.setObjectName(u"label_12")
+        self.label_12.setFont(font1)
+        self.label_12.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.label_12, 4, 4, 1, 1)
+
+        self.labelChecking_score = QLabel(self.page_check)
+        self.labelChecking_score.setObjectName(u"labelChecking_score")
+        self.labelChecking_score.setFont(font3)
+        self.labelChecking_score.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.labelChecking_score, 1, 0, 1, 1)
+
+        self.label_10 = QLabel(self.page_check)
+        self.label_10.setObjectName(u"label_10")
+        self.label_10.setFont(font1)
+        self.label_10.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.label_10, 4, 1, 1, 1)
+
+        self.label_8 = QLabel(self.page_check)
+        self.label_8.setObjectName(u"label_8")
+        self.label_8.setFont(font1)
+        self.label_8.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.label_8, 2, 4, 1, 1)
+
+        self.label_7 = QLabel(self.page_check)
+        self.label_7.setObjectName(u"label_7")
+        self.label_7.setFont(font1)
+        self.label_7.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.label_7, 2, 2, 1, 1)
+
+        self.label_11 = QLabel(self.page_check)
+        self.label_11.setObjectName(u"label_11")
+        self.label_11.setFont(font1)
+        self.label_11.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.label_11, 4, 2, 1, 1)
+
+        self.label_4 = QLabel(self.page_check)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setFont(font1)
+        self.label_4.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.label_4, 0, 2, 1, 1)
+
+        self.label_1 = QLabel(self.page_check)
+        self.label_1.setObjectName(u"label_1")
+        self.label_1.setFont(font1)
+        self.label_1.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.label_1, 0, 0, 1, 1)
+
+        self.label_9 = QLabel(self.page_check)
+        self.label_9.setObjectName(u"label_9")
+        self.label_9.setFont(font1)
+        self.label_9.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.label_9, 4, 0, 1, 1)
+
+        self.labelChecking_nums = QLabel(self.page_check)
+        self.labelChecking_nums.setObjectName(u"labelChecking_nums")
+        self.labelChecking_nums.setFont(font3)
+        self.labelChecking_nums.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.labelChecking_nums, 5, 0, 1, 1)
+
+        self.labelChecking_eabits = QLabel(self.page_check)
+        self.labelChecking_eabits.setObjectName(u"labelChecking_eabits")
+        self.labelChecking_eabits.setFont(font3)
+        self.labelChecking_eabits.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.labelChecking_eabits, 1, 1, 1, 1)
+
+        self.labelChecking_weakFactor = QLabel(self.page_check)
+        self.labelChecking_weakFactor.setObjectName(u"labelChecking_weakFactor")
+        self.labelChecking_weakFactor.setFont(font3)
+        self.labelChecking_weakFactor.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.labelChecking_weakFactor, 1, 2, 1, 1)
+
+        self.labelChecking_length = QLabel(self.page_check)
+        self.labelChecking_length.setObjectName(u"labelChecking_length")
+        self.labelChecking_length.setFont(font3)
+        self.labelChecking_length.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.labelChecking_length, 1, 4, 1, 1)
+
+        self.labelChecking_uppercase = QLabel(self.page_check)
+        self.labelChecking_uppercase.setObjectName(u"labelChecking_uppercase")
+        self.labelChecking_uppercase.setFont(font3)
+        self.labelChecking_uppercase.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.labelChecking_uppercase, 3, 1, 1, 1)
+
+        self.labelChecking_lowercase = QLabel(self.page_check)
+        self.labelChecking_lowercase.setObjectName(u"labelChecking_lowercase")
+        self.labelChecking_lowercase.setFont(font3)
+        self.labelChecking_lowercase.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.labelChecking_lowercase, 3, 2, 1, 1)
+
+        self.labelChecking_specialcase = QLabel(self.page_check)
+        self.labelChecking_specialcase.setObjectName(u"labelChecking_specialcase")
+        self.labelChecking_specialcase.setFont(font3)
+        self.labelChecking_specialcase.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.labelChecking_specialcase, 3, 4, 1, 1)
+
+        self.labelChecking_repeated = QLabel(self.page_check)
+        self.labelChecking_repeated.setObjectName(u"labelChecking_repeated")
+        self.labelChecking_repeated.setFont(font3)
+        self.labelChecking_repeated.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.labelChecking_repeated, 5, 1, 1, 1)
+
+        self.labelChecking_combs = QLabel(self.page_check)
+        self.labelChecking_combs.setObjectName(u"labelChecking_combs")
+        self.labelChecking_combs.setFont(font3)
+        self.labelChecking_combs.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.labelChecking_combs, 5, 2, 1, 1)
+
+        self.labelChecking_chars = QLabel(self.page_check)
+        self.labelChecking_chars.setObjectName(u"labelChecking_chars")
+        self.labelChecking_chars.setFont(font3)
+        self.labelChecking_chars.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_check.addWidget(self.labelChecking_chars, 5, 4, 1, 1)
+
+
+        self.verticalLayout_3.addLayout(self.gridLayout_check)
+
+        self.pushButton_gohome = QPushButton(self.page_check)
+        self.pushButton_gohome.setObjectName(u"pushButton_gohome")
+        self.pushButton_gohome.setMinimumSize(QSize(0, 40))
+        self.pushButton_gohome.setFont(font)
+        self.pushButton_gohome.setCursor(QCursor(Qt.PointingHandCursor))
+
+        self.verticalLayout_3.addWidget(self.pushButton_gohome)
+
         self.stackedWidget.addWidget(self.page_check)
 
         self.verticalLayout.addWidget(self.stackedWidget)
@@ -114,6 +312,9 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
+        self.stackedWidget.setCurrentIndex(0)
+
+
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
@@ -124,6 +325,33 @@ class Ui_MainWindow(object):
         self.toolButton_copy.setText(QCoreApplication.translate("MainWindow", u"C", None))
         self.pushButton_generatePassword.setText(QCoreApplication.translate("MainWindow", u"Generate Password", None))
         self.pushButton_check.setText(QCoreApplication.translate("MainWindow", u"Check Password", None))
+        self.labelChecking_password.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Letters", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Entropybits", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"Uppercases", None))
+        self.label_13.setText(QCoreApplication.translate("MainWindow", u"Length", None))
+        self.labelChecking_letters.setText(QCoreApplication.translate("MainWindow", u"0", None))
+        self.label_12.setText(QCoreApplication.translate("MainWindow", u"Chars", None))
+        self.labelChecking_score.setText(QCoreApplication.translate("MainWindow", u"0", None))
+        self.label_10.setText(QCoreApplication.translate("MainWindow", u"Repeated\n"
+"Patterns", None))
+        self.label_8.setText(QCoreApplication.translate("MainWindow", u"Specialcases", None))
+        self.label_7.setText(QCoreApplication.translate("MainWindow", u"Lowecases", None))
+        self.label_11.setText(QCoreApplication.translate("MainWindow", u"Combinations", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Weak Factor", None))
+        self.label_1.setText(QCoreApplication.translate("MainWindow", u"Score", None))
+        self.label_9.setText(QCoreApplication.translate("MainWindow", u"Numbers", None))
+        self.labelChecking_nums.setText(QCoreApplication.translate("MainWindow", u"0", None))
+        self.labelChecking_eabits.setText(QCoreApplication.translate("MainWindow", u"0", None))
+        self.labelChecking_weakFactor.setText(QCoreApplication.translate("MainWindow", u"0", None))
+        self.labelChecking_length.setText(QCoreApplication.translate("MainWindow", u"0", None))
+        self.labelChecking_uppercase.setText(QCoreApplication.translate("MainWindow", u"0", None))
+        self.labelChecking_lowercase.setText(QCoreApplication.translate("MainWindow", u"0", None))
+        self.labelChecking_specialcase.setText(QCoreApplication.translate("MainWindow", u"0", None))
+        self.labelChecking_repeated.setText(QCoreApplication.translate("MainWindow", u"0", None))
+        self.labelChecking_combs.setText(QCoreApplication.translate("MainWindow", u"0", None))
+        self.labelChecking_chars.setText(QCoreApplication.translate("MainWindow", u"0", None))
+        self.pushButton_gohome.setText(QCoreApplication.translate("MainWindow", u"Home Page", None))
         self.menuMenu.setTitle(QCoreApplication.translate("MainWindow", u"Menu", None))
     # retranslateUi
 
